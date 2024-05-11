b4201a6c9697ec5c54f84aba08e80088e3cdc472
Kirill-Magankov <kirill.magankov@gmail.com>
2024-05-11 16:42:35 +0300

    Fixed metrics scoring

--- flsite.py
+++ flsite.py
@@ -1,9 +1,15 @@
 import math
 import pickle
 import warnings
+from enum import Enum, auto
 
 import numpy as np
+import pandas as pd
 from flask import Flask, render_template, url_for, request
+from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error, mean_absolute_error, r2_score, \
+    accuracy_score, precision_score, recall_score, f1_score
+
+from model.test_split import get_shoe_size_test_set, get_shoe_size_gender_test_set, get_diabetes_test_set
 
 app = Flask(__name__)
 
@@ -13,11 +19,64 @@ menu = [{"name": "Лаба 1", "url": "p_knn"},
         {"name": "Лаба 4", "url": "p_lab4"}]
 
 diabetes_model = pickle.load(open('model/diabetes.pickle', 'rb'))
+diabetes_tree_model = pickle.load(open('model/diabetes_decision_tree.pickle', 'rb'))
 gender_shoe_model = pickle.load(open('model/shoe-size-gender.pickle', 'rb'))
 shoe_model = pickle.load(open('model/shoe-size_predict.pickle', 'rb'))
 
 diabetes_status = ["нет", "есть"]
 gender_list = ["женский", "мужской"]
+
+
+class ModelTypes(Enum):
+    DIABETES = auto()
+    DIABETES_TREE = auto()
+    GENDER_SHOE = auto()
+    SHOE_MODEL = auto()
+
+
+def get_regression_metrics(model_type: ModelTypes):
+    if model_type == ModelTypes.SHOE_MODEL:
+        y_test, x_test = get_shoe_size_test_set()
+        y_pred = shoe_model.predict(x_test)  # noqa
+
+    else:
+        return None
+
+    return [{"title": "MSE", "value": round(mean_squared_error(y_test, y_pred), 4)},
+            {"title": "RMSE", "value": round(math.sqrt(mean_absolute_error(y_test, y_pred)), 4)},
+            {"title": "MSPE", "value": f"{round(np.mean(np.square((y_test - y_pred) / y_test)) * 100, 2)} %"},
+            {"title": "MAE", "value": round(mean_absolute_error(y_test, y_pred), 4)},
+            {"title": "MAPE", "value": f"{round(mean_absolute_percentage_error(y_test, y_pred), 2)} %"},
+            {"title": "MRE", "value": round(mean_squared_error(y_test, y_pred), 4)},
+            {"title": "R-Квадрат", "value": round(r2_score(y_test, y_pred), 4)}]
+
+
+def get_classification_metrics(model_type: ModelTypes):
+    if model_type == ModelTypes.GENDER_SHOE:
+        y_test, x_test = get_shoe_size_gender_test_set()
+        y_pred = gender_shoe_model.predict(x_test)  # noqa
+
+    elif model_type == ModelTypes.DIABETES:
+        y_test, x_test = get_diabetes_test_set()
+        y_pred = diabetes_model.predict(x_test)  # noqa
+
+    elif model_type == ModelTypes.DIABETES_TREE:
+        y_test, x_test = get_diabetes_test_set()
+        y_pred = diabetes_tree_model.predict(x_test)  # noqa
+
+    else:
+        return
+
+    frame = {'y_Actual': y_test, 'y_Predicted': y_pred}
+    df = pd.DataFrame(frame, columns=['y_Actual', 'y_Predicted'])
+    confusion_matrix = pd.crosstab(df['y_Actual'], df['y_Predicted'], rownames=['Actual'], colnames=['Predicted'])
+
+    return [{"title": "Confusion matrix", "value": confusion_matrix},
+            {"title": "Accuracy", "value": round(accuracy_score(y_test, y_pred), 4)},
+            {"title": "Precision", "value": round(precision_score(y_test, y_pred), 4)},
+            {"title": "Recall", "value": round(recall_score(y_test, y_pred), 4)},
+            {"title": "F1-мера", "value": round(f1_score(y_test, y_pred), 4)},
+            ]
 
 
 @app.route("/")
@@ -27,8 +86,11 @@ def index():
 
 @app.route("/p_knn", methods=['POST', 'GET'])
 def f_lab1():
+    metrics = get_classification_metrics(ModelTypes.GENDER_SHOE)
+
     if request.method == 'GET':
-        return render_template('lab1.html', title="Метод k -ближайших соседей (KNN)", menu=menu, class_model='')
+        return render_template('lab1.html', title="Метод k -ближайших соседей (KNN)", menu=menu, class_model='',
+                               metrics=metrics)
     if request.method == 'POST':
         X_new = np.array([[float(request.form['height']),
                            float(request.form['weight']),
@@ -36,11 +98,13 @@ def f_lab1():
 
         pred = gender_shoe_model.predict(X_new)
         return render_template('lab1.html', title="Метод k -ближайших соседей (KNN)", menu=menu,
-                               class_model=f"Пол: {gender_list[pred[0]]}")
+                               class_model=f"Пол: {gender_list[pred[0]]}", metrics=metrics)
 
 
 @app.route("/p_lab2", methods=['POST', 'GET'])
 def f_lab2():
+    metrics = get_regression_metrics(ModelTypes.SHOE_MODEL)
+
     if request.method == 'POST':
         X_new = np.array([[float(request.form['height']),
                            float(request.form['weight']),
@@ -49,13 +113,15 @@ def f_lab2():
         pred = shoe_model.predict(X_new)
 
         return render_template('lab2.html', title="Логистическая регрессия", menu=menu,
-                               class_model=f"Размер обуви: {math.ceil(pred[0])}")
+                               class_model=f"Размер обуви: {math.ceil(pred[0])}", metrics=metrics)
 
-    return render_template('lab2.html', title="Логистическая регрессия", menu=menu)
+    return render_template('lab2.html', title="Логистическая регрессия", menu=menu, metrics=metrics)
 
 
 @app.route("/p_lab3", methods=['POST', 'GET'])
 def f_lab3():
+    metrics = get_classification_metrics(ModelTypes.DIABETES)
+
     if request.method == 'POST':
         X_new = np.array([[float(request.form['pregnancies']),
                            float(request.form['glucose']),
@@ -67,13 +133,15 @@ def f_lab3():
 
         pred = diabetes_model.predict(X_new)
         return render_template('lab3.html', title="Логистическая регрессия", menu=menu,
-                               class_model=f"Диабет: {diabetes_status[pred[0]]}")
+                               class_model=f"Диабет: {diabetes_status[pred[0]]}", metrics=metrics)
 
-    return render_template('lab3.html', title="Логистическая регрессия", menu=menu)
+    return render_template('lab3.html', title="Логистическая регрессия", menu=menu, metrics=metrics)
 
 
 @app.route("/p_lab4", methods=['POST', 'GET'])
 def f_lab4():
+    metrics = get_classification_metrics(ModelTypes.DIABETES_TREE)
+
     if request.method == 'POST':
         X_new = np.array([[float(request.form['pregnancies']),
                            float(request.form['glucose']),
@@ -83,11 +151,11 @@ def f_lab4():
                            float(request.form['bmi']),
                            float(request.form['age'])]])
 
-        pred = diabetes_model.predict(X_new)
+        pred = diabetes_tree_model.predict(X_new)
         return render_template('lab4.html', title="Древо решений", menu=menu,
-                               class_model=f"Диабет: {diabetes_status[pred[0]]}")
+                               class_model=f"Диабет: {diabetes_status[pred[0]]}", metrics=metrics)
 
-    return render_template('lab4.html', title="Древо решений", menu=menu)
+    return render_template('lab4.html', title="Древо решений", menu=menu, metrics=metrics)
 
 
 if __name__ == "__main__":
---
+++ model/test_split.py
@@ -0,0 +1,31 @@
+import os
+import pandas as pd
+from pandas import Series
+from sklearn.model_selection import train_test_split
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
+
+def get_shoe_size_test_set():
+    dataset_df = pd.read_csv(rf'{current_dir}\datasets\shoe-size.csv')
+    X = dataset_df.drop(['Shoe size'], axis=1)
+    y = dataset_df['Shoe size']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+    return y_test, X_test
+
+
+def get_shoe_size_gender_test_set():
+    dataset_df = pd.read_csv(rf'{current_dir}\datasets\shoe-size.csv')
+    X = dataset_df.drop(['Gender'], axis=1)
+    y = dataset_df['Gender']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+    return y_test, X_test
+
+
+def get_diabetes_test_set():
+    dataset_df = pd.read_csv(rf'{current_dir}\datasets\diabetes.csv')
+    dataset_df = dataset_df.drop(["DiabetesPedigreeFunction"], axis=1)
+    X = dataset_df.drop(['Outcome'], axis=1)
+    y = dataset_df['Outcome']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+    return y_test, X_test
